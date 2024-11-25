d9b95770176126eafdee5bc024eafdc7943d5f00
Claudio Pi <claudiopi@gmail.com>
2024-11-25 16:50:03 -0300

    Task Management initial version

---
+++ src/app/kanban-view/kanban-view.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kanban-view',
+  templateUrl: './kanban-view.component.html',
+  styleUrls: ['./kanban-view.component.scss']
+})
+export class KanbanViewComponent {}
